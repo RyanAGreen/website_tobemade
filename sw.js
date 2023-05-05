730052e3166f2769595db6fc12c55f025efb9c20
Ryan Green <rygreen@ucsc.edu>
2023-05-05 15:38:24 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,27 +27,27 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4b9a4676c0847aadfdd9.js"
+    "url": "webpack-runtime-efddc2873663b29458d3.js"
   },
   {
-    "url": "framework-0f8de3526885dd073c5e.js"
+    "url": "framework-cfc39a90b11063613693.js"
   },
   {
-    "url": "app-c00a624ce817d161cc23.js"
+    "url": "app-ab16277f21938c0c1ee8.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8244c98e2dd8a59b496bc5b8f0429132"
+    "revision": "4a31c0b40ce26daa94ef85b2ba07420c"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f277fb9b28e1661bc3bb.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b5bc8a925215f7166f76.js"
   },
   {
-    "url": "polyfill-633d156253342d03a581.js"
+    "url": "polyfill-14edfb9d06b6becd98b0.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "91cc892626d0714c8ceca40142523086"
+    "revision": "c90c7212abf56760a021422f262e807d"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-c00a624ce817d161cc23.js`))) {
+  if (!resources || !(await caches.match(`/app-ab16277f21938c0c1ee8.js`))) {
     return await fetch(event.request)
   }
 
